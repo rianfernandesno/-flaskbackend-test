91fe2416f82cc910cfa8bd1c00b9b5eb5fd1969b
rianfernandesno <zrpapz12@gmail.com>
2025-07-07 23:56:56 -0300

    Templates

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, world"
+def home():
+    return render_template('home.html')
+
+@app.route('/contato')
+def contat():
+    return render_template('contato.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
